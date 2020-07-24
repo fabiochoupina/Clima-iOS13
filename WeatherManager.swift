61086da3d6632b49717ea941c6d975ec38b45de9
Fabio <soaresfabiojr@gmail.com>
2020-07-25 09:34:23 +1200

    parse json data using JSONDecoder

--- WeatherManager.swift
+++ WeatherManager.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=d32b4f192dbcf054cbfc3e5fada1b26b&q=London"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=d32b4f192dbcf054cbfc3e5fada1b26b&q=London&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
@@ -30,14 +30,23 @@ struct WeatherManager {
                     print(error!)
                     return
                 }
-                
                 if let safeData = data {
-                    let dataString = String(data: safeData, encoding: .utf8)
-                    print(dataString)
+                    self.parseJSON(weatherData: safeData)
                 }
             }
             //4. Start the task
             task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            print(decodedData.main.temp)
+            print(decodedData.weather[0].description)
+        } catch {
+            print(error)
         }
     }
 }
